fb5fa5cce4ee7f62751d81e0258cd0fe89927b5a
kaushikrao77 <kaushikrao77@gmail.com>
2020-08-16 00:56:14 +0530

    loader and feedback

--- public/login.js
+++ public/login.js
@@ -1,30 +1,50 @@
 let formButton = document.querySelector(".subm");
 
-formButton.addEventListener("click",(e)=>{
-    e.preventDefault();
-    let data = {
-        email: document.querySelectorAll(".text-inp")[0].value,
-        password: document.querySelectorAll(".text-inp")[1].value,
-    };
-    var requestOptions = {
-        method: 'post',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        redirect: 'follow',
-        body: JSON.stringify(data)
+formButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelectorAll(".feed-text").forEach((x) => {
+    x.innerHTML = "";
+  });
+  let data = {
+    email: document.querySelectorAll(".text-inp")[0].value,
+    password: document.querySelectorAll(".text-inp")[1].value,
   };
-    fetch('/member/login',requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            if(data.code == 403){
-                console.log("Bad request")
-            }
-            else{
-                sessionStorage.setItem("token",data.token);
-                console.log("Hellso")
-                window.location = "http://localhost:5000/member/edit";
-            }
-        })
-
-})
+  if (data.email === "") {
+    document.querySelector(".e-text .feed-text").innerHTML =
+      "This field is required";
+    return;
+  }
+  if (
+    !data.email.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    document.querySelector(".e-text .feed-text").innerHTML =
+      "Enter a valid email address";
+    return;
+  }
+  if (data.password === "") {
+    document.querySelector(".p-text .feed-text").innerHTML =
+      "This field is required";
+    return;
+  }
+  var requestOptions = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify(data),
+  };
+  fetch("/member/login", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.code == 403) {
+        console.log("Bad request");
+      } else {
+        sessionStorage.setItem("token", data.token);
+        console.log("Hellso");
+        window.location = "http://localhost:5000/member/edit";
+      }
+    });
+});
--- public/register.js
+++ public/register.js
@@ -1,33 +1,78 @@
 let formSubmit = document.querySelector(".subm");
-let token = sessionStorage.getItem("adminToken");
+// let token = sessionStorage.getItem("adminToken");
 
-formSubmit.addEventListener("click",(e)=>{
-    e.preventDefault();
-    console.log("click")
-    let data = {
-        email: document.querySelector(".text-inp-email").value,
-        name: document.querySelector(".text-inp-name").value,
-        buisnessName: document.querySelector(".text-inp-bname").value,
-        title: document.querySelector(".text-inp-bname").value,
-        phone: document.querySelector(".text-inp-phone").value,
-        password: document.querySelector(".text-inp-password").value,
-        slug: document.querySelector(".text-inp-route").value,
-    }
-if(token){
-    fetch("/admin/register",{
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type" : "application/json",
-            "Authorization" : `Bearer ${token}`
-        },
-        mode: "cors"
-    }).then(data=>data.text())
-    .then(token=>{
-           console.log(token);
+formSubmit.addEventListener("click", (e) => {
+  document.querySelectorAll(".feed-text").forEach((x) => {
+    x.innerHTML = "";
+  });
+  e.preventDefault();
+  console.log("click");
+  let data = {
+    email: document.querySelector(".text-inp-email").value,
+    name: document.querySelector(".text-inp-name").value,
+    buisnessName: document.querySelector(".text-inp-bname").value,
+    title: document.querySelector(".text-inp-bname").value,
+    phone: document.querySelector(".text-inp-phone").value,
+    password: document.querySelector(".text-inp-password").value,
+    cpassword: document.querySelector(".text-inp-cpassword").value,
+    slug: document.querySelector(".text-inp-route").value,
+  };
+  if (
+    !data.email.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    document.querySelector(".em-text .feed-text").innerHTML =
+      "Enter a valid email address";
+    return;
+  }
+  if (data.buisnessName === "") {
+    document.querySelector(".bn-text .feed-text").innerHTML =
+      "This field is required";
+    return;
+  }
+  if (data.name === "") {
+    document.querySelector(".n-text .feed-text").innerHTML =
+      "This field is required";
+    return;
+  }
+  if (data.slug === "") {
+    document.querySelector(".sl-text .feed-text").innerHTML =
+      "This field is required";
+    return;
+  }
+  if (data.password === "") {
+    document.querySelector(".p-text .feed-text").innerHTML =
+      "This field is required";
+    return;
+  }
+  if (!data.phone.match(/^\d+$/)) {
+    document.querySelector(".ph-text .feed-text").innerHTML =
+      "This field should contain only digits";
+    return;
+  }
+  if (data.password !== data.cpassword) {
+    document.querySelector(".cp-text .feed-text").innerHTML =
+      "Password doesn't match";
+
+    return;
+  }
+  if (token) {
+    fetch("/admin/register", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      mode: "cors",
+    })
+      .then((data) => data.text())
+      .then((token) => {
+        console.log(token);
         //    sessionStorage.setItem("token",`${token}`);
-           window.location = '/admin';
-       })
-       .catch(err=>console.log(err));
-    }
+        window.location = "/admin";
+      })
+      .catch((err) => console.log(err));
+  }
 });
