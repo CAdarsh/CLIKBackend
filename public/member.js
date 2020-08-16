aacae5a72d8485778eb7a4f369c53de704e205f2
kaushikrao77 <kaushikrao77@gmail.com>
2020-08-16 17:39:23 +0530

    multer bug

--- public/member.js
+++ public/member.js
@@ -9,8 +9,6 @@ document.querySelector(".edit-button").addEventListener("click", () => {
     edit.innerHTML = "Submit";
     document.querySelector(".show-upload").style.display = "grid";
   } else {
-    
-
     let title = document.querySelector(".info-name").innerHTML;
     let content = document.querySelector(".info-actual-text").innerHTML;
     let location = document.querySelector(".location").innerHTML;
@@ -18,31 +16,53 @@ document.querySelector(".edit-button").addEventListener("click", () => {
     let bPhone = document.querySelector(".phone").value;
     let bEmail = document.querySelector(".email").value;
     let slug = document.querySelector(".slug").value;
-    let sendingObject = { slug,content, bEmail, bPhone, website, title, location };
-    //do fetch here
-    let token = sessionStorage.getItem("token");
-      if (token && token != "undefined") {
-        console.log("yes" + token);
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${token}`);
-        myHeaders.append("Content-Type",'application/json');
-        var requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: JSON.stringify(sendingObject),
-          redirect: "follow",
-        };
+    let imageFile = document.querySelector(".image-file-up");
 
-        fetch("/member/details", requestOptions)
-          .then((response) => response.json())
-          .then((result) => {
-            ChangeData(result);
-          })
-          .then(()=>{window.location = '/member/edit'})
-          .catch((error) => console.log("error", error));
-      } else {
-        window.location = "/member";
-      }
+    let formData = new FormData();
+
+    formData.append("myImage", imageFile.files[0]);
+    formData.append("title", JSON.stringify(title));
+    formData.append("content", content);
+    formData.append("location", location);
+    formData.append("website", website);
+    formData.append("bPhone", bPhone);
+    formData.append("bEmail", bEmail);
+    formData.append("slug", slug);
+    // let sendingObject = {
+    //   slug,
+    //   content,
+    //   bEmail,
+    //   bPhone,
+    //   website,
+    //   title,
+    //   location,
+    //   formData,
+    // };
+    let token = sessionStorage.getItem("token");
+    if (token && token != "undefined") {
+      console.log("yes" + token);
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+      // myHeaders.append("Content-Type", "application/json");
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: formData,
+        // redirect: "follow",
+      };
+      delete requestOptions.headers["Content-Type"];
+      fetch("/member/details", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          ChangeData(result);
+        })
+        .then(() => {
+          window.location = "/member/edit";
+        })
+        .catch((error) => console.log("error", error));
+    } else {
+      window.location = "/member";
+    }
     document.designMode = "off";
     document.querySelector(".inp-tags").style.display = "none";
     edit.innerHTML = "Edit";
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,111 +1,149 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+let multer = require("multer");
+let storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+let upload = multer({
+  storage: storage,
+}).single("myImage");
 
 const router = express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
-const { auth,authAndUpdate,adminAuth } = require("../middlewares");
+const { auth, authAndUpdate, adminAuth } = require("../middlewares");
 const registerController = require("../controllers/register");
 const loginController = require("../controllers/loginController");
 const getDetails = require("../controllers/getDetails");
-const { addAdmin,sendAdmin,deleteAdmin,loginAdmin } = require("../controllers/admin");
+const {
+  addAdmin,
+  sendAdmin,
+  deleteAdmin,
+  loginAdmin,
+} = require("../controllers/admin");
 
+router.use("/member", express.static("public"));
+router.use("/admin", express.static("public"));
+router.use("/page", express.static("public"));
 
-router.use('/member',express.static('public'));
-router.use('/admin',express.static('public'));
-router.use('/page',express.static('public'));
+router.use(function (req, res, next) {
+  res.locals.logout = false;
+
+  next();
+});
 
 //admin
-router.get('/admin',(req,res)=>{
-    res.render('pages/dashboard.ejs');
-})
+router.get("/admin", (req, res) => {
+  res.render("pages/dashboard.ejs");
+});
 
 // router.get('/admin/add',async (req,res)=>{
-//   let result = await addAdmin(req.body); 
+//   let result = await addAdmin(req.body);
 //   res.json(result)
 // })
 
-router.get('/admin/all', adminAuth,async (req,res)=>{
-  let result = await sendAdmin(); 
-  res.json(result)
-})
-
-router.get('/admin/delete', adminAuth,async (req,res)=>{
-  let result = await deleteAdmin(); 
-  res.json(result)
-})
-
-
-router.get('/admin/login',async (req,res)=>{
-  res.render('pages/adminLogin.ejs');
-})
-
-router.post('/admin/login',async (req,res)=>{
-  let result = await loginAdmin(req.body); 
-  res.json(result)
-})
-
-router.get('/admin/register', (req, res) => {
-  res.render('pages/register.ejs', {isError: false });
+router.get("/admin/all", adminAuth, async (req, res) => {
+  let result = await sendAdmin();
+  res.json(result);
 });
 
-router.post('/admin/register', adminAuth, async (req, res) => {
-  if(req.data){
+router.get("/admin/delete", adminAuth, async (req, res) => {
+  let result = await deleteAdmin();
+  res.json(result);
+});
+
+router.get("/admin/login", async (req, res) => {
+  res.render("pages/adminLogin.ejs");
+});
+
+router.post("/admin/login", async (req, res) => {
+  let result = await loginAdmin(req.body);
+  res.json(result);
+});
+
+router.get("/admin/register", (req, res) => {
+  res.render("pages/register.ejs", { isError: false });
+});
+
+router.post("/admin/register", adminAuth, async (req, res) => {
+  if (req.data) {
     let token = await registerController.addNewUser(req.body);
-    res.send("1");  
-  }
-  else{
+    res.send("1");
+  } else {
     res.send("0");
   }
-  });
+});
 
 //members area
 
-router.get('/member',(req,res)=>{
-  res.render('pages/login.ejs');
-})
-
-router.get('/member/edit',(req, res) => {
-  res.render('pages/member.ejs');
+router.get("/member", (req, res) => {
+  res.render("pages/login.ejs");
 });
 
-router.get('/member/all', async (req, res) => {
+router.get("/member/edit", (req, res) => {
+  res.render("pages/member.ejs", { logout: true });
+  // console.log(res);
+});
+
+router.get("/member/all", async (req, res) => {
   let result = await registerController.returnUsers();
-  console.log(req.headers)
+  console.log(req.headers);
   res.send(result);
 });
-router.get('/member/details', auth, (req, res) => {
-  console.log(req.dataJWT)
+router.get("/member/details", auth, (req, res) => {
+  // console.log(req.body);
   res.send(req.dataJWT);
 });
-router.post('/member/details', authAndUpdate, (req, res) => {
-  console.log(req.data)
-  res.json(req.data);
-});
-router.post('/member/login', async (req, res) => {
+router.post(
+  "/member/details",
+  // authAndUpdate,
+  (req, res) => {
+    console.log(req.body + "this is boooooooooody");
+    upload(req, res, (err) => {
+      if (err) {
+        res.send(err);
+        console.log(err + "this is the eeeeeeeeeerrorr");
+      } else {
+        console.log("req.file=====" + req.file);
+      }
+    });
+    // console.log("req.body===========" + req.file);
+    // res.json(req.data);
+  }
+);
+router.post("/member/login", async (req, res) => {
   let response = await loginController.loginUser(req.body);
-  console.log(response,req.body)
+  console.log(response, req.body);
   res.json(response);
 });
 
 //public
-router.get('/page/:slug', (req, res)=>{
-
-  res.render('pages/memberDisplay.ejs',{slug:req.params.slug});
+router.get("/page/:slug", (req, res) => {
+  res.render("pages/memberDisplay.ejs", {
+    slug: req.params.slug,
+  });
 });
 
-router.post('/page/details',async (req, res)=>{
+router.post("/page/details", async (req, res) => {
   let result = await getDetails.getUser(req.body.slug);
   console.log(result);
   res.json(result);
 });
 
-
-router.get('/', (req, res) => {
-  res.render('pages/members.ejs');
+router.get("/", (req, res) => {
+  res.render("pages/members.ejs");
 });
 
-router.get('/404', (req, res) => {
-  res.render('pages/members.ejs');
+router.get("/404", (req, res) => {
+  res.render("pages/members.ejs");
 });
-
 
 module.exports = router;
