1006d1b137bbd3d0f3965ce1dfd1ccd75c0c4c06
cadarsh <cadarsh335@gmail.com>
2020-08-19 13:30:44 +0530

    Errors handled

--- public/login.js
+++ public/login.js
@@ -1,17 +1,16 @@
-let formButton = document.querySelector(".subm");
+const formButton = document.querySelector('.subm');
 
-formButton.addEventListener("click", (e) => {
+formButton.addEventListener('click', (e) => {
   e.preventDefault();
-  document.querySelectorAll(".feed-text").forEach((x) => {
-    x.innerHTML = "";
+  document.querySelectorAll('.feed-text').forEach((x) => {
+    x.innerHTML = '';
   });
-  let data = {
-    email: document.querySelectorAll(".text-inp")[0].value,
-    password: document.querySelectorAll(".text-inp")[1].value,
+  const data = {
+    email: document.querySelectorAll('.text-inp')[0].value,
+    password: document.querySelectorAll('.text-inp')[1].value,
   };
-  if (data.email === "") {
-    document.querySelector(".e-text .feed-text").innerHTML =
-      "This field is required";
+  if (data.email === '') {
+    document.querySelector('.e-text .feed-text').innerHTML = 'This field is required';
     return;
   }
   if (
@@ -19,32 +18,33 @@ formButton.addEventListener("click", (e) => {
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     )
   ) {
-    document.querySelector(".e-text .feed-text").innerHTML =
-      "Enter a valid email address";
+    document.querySelector('.e-text .feed-text').innerHTML = 'Enter a valid email address';
     return;
   }
-  if (data.password === "") {
-    document.querySelector(".p-text .feed-text").innerHTML =
-      "This field is required";
+  if (data.password === '') {
+    document.querySelector('.p-text .feed-text').innerHTML = 'This field is required';
     return;
   }
-  var requestOptions = {
-    method: "post",
+  const requestOptions = {
+    method: 'post',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    redirect: "follow",
+    redirect: 'follow',
     body: JSON.stringify(data),
   };
-  fetch("/member/login", requestOptions)
+
+  fetch('/member/login', requestOptions)
     .then((response) => response.json())
     .then((data) => {
-      if (data.code == 403) {
-        console.log("Bad request");
-      } else {
-        sessionStorage.setItem("token", data.token);
-        console.log("Hellso");
-        window.location = "http://localhost:5000/member/edit";
+      if (data.status == 403) {
+        alert('Bad request');
+        document.querySelector('.feed-text-error').innerHTML = 'Invalid email and password';
+      } else if (data.status == 200) {
+        alert(data);
+        sessionStorage.setItem('token', data.token);
+        console.log('Hellso');
+        window.location = 'http://localhost:5000/member/edit';
       }
     });
 });
